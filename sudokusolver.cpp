29c1e83cde70907ae13f3a12a1c6e286eadecf62
Rathore13055 <kgaurav1412005@gmail.com>
2024-12-15 12:58:18 +0530

    Add files via upload

---
+++ sudokusolver.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+#define N 9
+
+void printGrid(int grid[N][N]) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            cout << grid[row][col] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool isValid(int grid[N][N], int row, int col, int num) {
+    for (int x = 0; x < N; x++) {
+        if (grid[row][x] == num || grid[x][col] == num) {
+            return false;
+        }
+    }
+
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i + startRow][j + startCol] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(int grid[N][N]) {
+    int row = -1, col = -1;
+    bool isEmpty = false;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (grid[i][j] == 0) {
+                row = i;
+                col = j;
+                isEmpty = true;
+                break;
+            }
+        }
+        if (isEmpty) break;
+    }
+
+    if (!isEmpty) return true;
+
+    for (int num = 1; num <= 9; num++) {
+        if (isValid(grid, row, col, num)) {
+            grid[row][col] = num;
+
+            if (solveSudoku(grid)) return true;
+
+            grid[row][col] = 0;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int grid[N][N];
+
+    cout << "Enter the Sudoku puzzle (use 0 for empty cells):" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    if (solveSudoku(grid)) {
+        cout << "Solved Sudoku:\n";
+        printGrid(grid);
+    } else {
+        cout << "No solution exists!";
+    }
+
+    return 0;
+}
